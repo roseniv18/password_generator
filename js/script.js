6b0a8c0e42960114c7f19c0a90895e34c586637f
Avitohol1 <87610719+Avitohol1@users.noreply.github.com>
2023-03-05 19:48:29 +0200

    Added regex for strong or medium strength password

--- js/script.js
+++ js/script.js
@@ -92,6 +92,9 @@ const randomFunc = {
     symbol: getRandomSymbol
 }
 
+const strongPass = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/g
+const mediumPass = /((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))/
+
 // Change slider text on change
 passwordLengthSliderEl.addEventListener('input', (e) => {
     passwordLengthEl.innerText = (e.target.value)
